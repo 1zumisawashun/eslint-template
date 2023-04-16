499aeea7dd13e69fbbd8034bd6437d08fb3b2567
1zumisawashun <shun.1zumisawa@gmail.com>
2023-04-16 18:51:04 +0900

    fix: :memo: guiでコンベンショナルコミットができるかの検証

--- src/components/SuperSlowComponent.tsx
+++ src/components/SuperSlowComponent.tsx
@@ -2,6 +2,7 @@ export const SuperSlowComponent = () => {
   // コメントアウトを追加する
   // さらにコメントアウトを追加する
   // 追加でコメントアウト
+  // コミット
   const now = performance.now()
   /* eslint-disable */
   while (performance.now() - now < 200) {}
